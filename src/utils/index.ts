0e6a0716252db39f2c9027a87952d9bdf544d1cd
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-01 19:08:39 +0900

    feat: convertTimeStampToTime util 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,29 @@ export const shuffle = (array: string[]) => {
 export const decodeString = (encodedString: string) => {
   return window.atob(encodedString);
 };
+
+const getPadNumberString = (value: number) => {
+  return value < 10 ? String(value).padStart(2, "0") : value.toString();
+};
+
+export const convertTimeStampToTime = (
+  timestamp: number,
+  currentTimestamp: number
+) => {
+  const SECOND = 1000;
+  const MINUTE = 60 * SECOND;
+  const HOUR = 60 * MINUTE;
+
+  const diff = currentTimestamp - timestamp;
+  const hours = Math.trunc(diff / HOUR);
+
+  const minDiff = diff - HOUR * hours;
+  const minutes = Math.trunc(minDiff / MINUTE);
+
+  const secondDiff = minDiff - MINUTE * minutes;
+  const seconds = Math.trunc(secondDiff / SECOND);
+
+  return `${getPadNumberString(hours)}:${getPadNumberString(
+    minutes
+  )}:${getPadNumberString(seconds)}`;
+};

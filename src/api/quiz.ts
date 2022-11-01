a91d97246d73bd3f4c2a2f4376dbf15719f1f1f3
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-01 14:15:32 +0900

    feat: quiz api 추가

--- src/api/quiz.ts
+++ src/api/quiz.ts
@@ -1,2 +1,22 @@
-// todo: fetch api
+import axios from "axios";
+
+type RequestParams = {
+  amount: number;
+  difficulty: string;
+};
+
+export const getQuiz = async (params: RequestParams) => {
+  try {
+    const response = await axios.get(
+      `https://opentdb.com/api.php?amount=${[params.amount]}&difficulty=${
+        params.difficulty
+      }&type=multiple`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("get quiz api error: ", error);
+    return [];
+  }
+};
+
 export default {};

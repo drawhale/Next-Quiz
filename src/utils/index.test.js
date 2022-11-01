c48fc806fbf35267f1a9fc375281e4b371ab462f
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-01 19:08:59 +0900

    chore: 테스트 코드 추가

---
+++ src/utils/index.test.js
@@ -0,0 +1,17 @@
+import { convertTimeStampToTime, shuffle } from "utils";
+
+describe("utils", () => {
+  test("shuffle(array) - should be equal length", () => {
+    const array = ["a", "b", "c"];
+    const newArray = shuffle(array);
+    expect(newArray.length).toBe(array.length);
+  });
+
+  test("convertTimeStampToTime(timestamp)) - should be convert hh:mm:ss", () => {
+    const timestamp1 = new Date("2022-11-01 18:00:00").getTime();
+    const timestamp2 = new Date("2022-11-01 19:02:31").getTime();
+
+    const result = convertTimeStampToTime(timestamp1, timestamp2);
+    expect(result).toBe("01:02:31");
+  });
+});

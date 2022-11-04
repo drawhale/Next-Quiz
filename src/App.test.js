96b903d5964f66615240cae29260a1d2f102feb8
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-04 22:27:22 +0900

    test: App 테스트 코드 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import { QUIZ_DIFFICULTYS } from "model/Quiz";
 
-test("renders learn react link", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/Next Quiz__/i);
-  // expect(linkElement).toBeInTheDocument();
-  expect(1).toBe(1);
+test("renders App", async () => {
+  render(<App />);
+
+  const mainButtonElement = screen.getByText("퀴즈 풀기");
+  expect(mainButtonElement).toBeInTheDocument();
+  fireEvent.click(mainButtonElement);
+
+  expect(screen.getByText("퀴즈의 난이도를 선택해주세요.")).toBeInTheDocument();
+  expect(screen.queryByText("시작하기")).not.toBeInTheDocument();
+
+  const difficultyButtonElement = screen.getByText(QUIZ_DIFFICULTYS[0]);
+  fireEvent.click(difficultyButtonElement);
+
+  const startQuizButtonElement = screen.queryByText("시작하기");
+  expect(startQuizButtonElement).toBeInTheDocument();
+  fireEvent.click(startQuizButtonElement);
+
+  expect(screen.getByText(/^퀴즈 만드는 중/)).toBeInTheDocument();
+
+  expect(await screen.findByText("QUESTION")).toBeVisible();
 });

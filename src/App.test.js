708aae3a1f69fa8e676148dd4621960a841300bd
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-05 18:21:12 +0900

    test: api mocking 및 App.test 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,20 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import App from "./App";
 import { QUIZ_DIFFICULTYS } from "model/Quiz";
+import { getQuizMockResponse } from "api/mock";
+import { decodeString } from "utils";
+
+const server = setupServer(
+  rest.get("https://opentdb.com/api.php", (req, res, ctx) => {
+    return res(ctx.json(getQuizMockResponse()));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test("renders App", async () => {
   render(<App />);
@@ -22,4 +36,19 @@ test("renders App", async () => {
   expect(screen.getByText(/^퀴즈 만드는 중/)).toBeInTheDocument();
 
   expect(await screen.findByText("QUESTION")).toBeVisible();
+
+  const quizMockData = getQuizMockResponse();
+
+  for (const item of quizMockData.results) {
+    const question = decodeString(item.question);
+    const correctAnswer = decodeString(item.correct_answer);
+    expect(await screen.findByText(question)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(correctAnswer));
+
+    const nextButtonElement = await screen.findByText("다음 문항");
+    expect(nextButtonElement).toBeVisible();
+    fireEvent.click(nextButtonElement);
+  }
+
+  expect(await screen.findByText("다시 풀기")).toBeInTheDocument();
 });
